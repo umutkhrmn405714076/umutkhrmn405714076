11225c63f34f96e504871a548713a89cc3857eb5
umutkhrmn405714076 <101084367+umutkhrmn405714076@users.noreply.github.com>
2022-03-07 09:09:06 +0300

    proje maaş takibi
    proje maaş takibi örnekler

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,36 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace Queue_Uygulama_1
+namespace personel_maaş_takibi
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
-            InitializeComponent();
-        }
-        Queue kuyruk = new Queue();
-        int sira = 0;
-        private void btnSiraAl_Click(object sender, EventArgs e)
-        {
-            sira++;
-            kuyruk.Enqueue(sira);
-            Listele();
+            
         }
 
-        private void Listele()
+        private void btnHesapla_Click(object sender, EventArgs e)
         {
-            ListeSiralar.Items.Clear();
-            foreach (int sira in kuyruk)
-            {
-                ListeSiralar.Items.Add(sira);
-            }
-        }
 
-        private void btnSiraCikart_Click(object sender, EventArgs e)
-        {
-            kuyruk.Dequeue();
-            Listele();
         }
     }
 }
